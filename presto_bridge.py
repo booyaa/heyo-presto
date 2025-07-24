3867c6861fc0c3fd1c0e3b806e5ebc319f2d2402
booyaa <192864+booyaa@users.noreply.github.com>
2025-07-24 16:47:11 +0100

    feat: receive messages

--- presto_bridge.py
+++ presto_bridge.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from meshtastic import tcp_interface
+from pubsub import pub
+
 
 app = Flask(__name__)
 meshtastic_node_host = os.getenv('MESHTASTIC_HOST', 'meshtastic.local')
@@ -17,3 +19,36 @@ def send_message(message):
         return {"status": "error", "message": str(e)}
     
     return {"status": "success", "message": message}
+
+@app.route("/get/message", methods=['GET'])
+def get_message():
+    if messages:
+        return {"status": "success", "message": messages.pop(0)}
+    else:
+        return {"status": "error", "message": "No messages available"}
+    
+def on_receive(packet, interface):
+    try:
+        if packet.get('decoded', {}).get('portnum') == 'TEXT_MESSAGE_APP':
+            raw_text = packet['decoded']['text']
+            if raw_text.lower().startswith('presto'):
+                print(f"DEBUG|{packet['decoded']['text']}")
+                messages.append(truncate_message(clean_up_message(raw_text)))
+            else:
+                print(f"DEBUG|Ignored: {raw_text}")
+    except Exception as e:
+        print(f"Error processing packet: {e}")
+
+def clean_up_message(message):
+    if message.lower().startswith('presto'):
+        message = message[6:].strip()
+        if message.startswith(':'):
+            message = message[1:].strip()
+    return message
+
+def truncate_message(message):
+    if len(message) > 22:
+        return message[:22] + "..."
+    return message
+
+pub.subscribe(on_receive, "meshtastic.receive")
