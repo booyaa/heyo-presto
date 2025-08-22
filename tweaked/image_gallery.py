2d8f7b1e844385c3d6aa376f4121c3a5d7654380
booyaa <192864+booyaa@users.noreply.github.com>
2025-08-22 13:59:46 +0100

    chore: pull in tweaked example

---
+++ tweaked/image_gallery.py
@@ -0,0 +1,255 @@
+# ICON [[(-6.0, 2.0), (14.0, 2.0), (7.1, -7.0), (2.5, -1.0), (-0.6, -5.0), (-6.0, 2.0)], [(-8.0, 10.0), (-8.77, 9.93), (-10.0, 9.49), (-10.81, 8.88), (-11.53, 7.91), (-11.87, 7.04), (-12.0, 6.05), (-12.0, -18.0), (-11.72, -19.5), (-11.15, -20.47), (-10.47, -21.14), (-9.78, -21.58), (-8.86, -21.91), (-8.05, -22.0), (16.0, -22.0), (16.82, -21.92), (18.02, -21.44), (18.51, -21.07), (19.42, -20.05), (19.83, -19.17), (20.0, -18.05), (20.0, 6.0), (19.7, 7.54), (19.37, 8.16), (18.65, 8.99), (17.36, 9.77), (16.05, 10.0), (-8.0, 10.0)], [(-8.0, 6.0), (16.0, 6.0), (16.0, -18.0), (-8.0, -18.0), (-8.0, 6.0)], [(-16.0, 18.0), (-16.73, 17.94), (-17.83, 17.58), (-18.81, 16.88), (-19.26, 16.35), (-19.79, 15.31), (-20.0, 14.05), (-20.0, -14.0), (-16.0, -14.0), (-16.0, 14.0), (12.0, 14.0), (12.0, 18.0), (-16.0, 18.0)], [(-8.0, -18.0), (-8.0, -18.0)]]
+# NAME Photo Frame
+# DESC A touch enabled image gallery
+
+'''
+An image gallery demo to turn your Pimoroni Presto into a desktop photo frame!
+
+- Create a folder called 'gallery' on the root of your SD card and fill it with JPEGs.
+- The image will change automatically every 5 minutes
+- You can also tap the right side of the screen to skip next image and left side to go to the previous :)
+
+'''
+import os
+import time
+
+import jpegdec
+import machine
+import plasma
+import sdcard
+import uos
+from presto import Presto
+
+# The total number of LEDs to set, the Presto has 7
+NUM_LEDS = 7
+
+# Seconds between changing the image on screen
+# This interval shows us a new image every 5 minutes
+INTERVAL = 60 * 5
+
+LEDS_LEFT = [4, 5, 6]
+LEDS_RIGHT = [0, 1, 2]
+
+# Setup for the Presto display
+presto = Presto()
+display = presto.display
+WIDTH, HEIGHT = display.get_bounds()
+
+BACKGROUND = display.create_pen(1, 1, 1)
+WHITE = display.create_pen(255, 255, 255)
+BLACK = display.create_pen(0, 0, 0)
+
+# We'll need this for the touch element of the screen
+touch = presto.touch
+
+# JPEG Dec
+j = jpegdec.JPEG(display)
+
+# Where our images are located
+directory = 'gallery'
+
+# Stores the total number of images in the user gallery
+total_image_count = 0
+
+# Store our current location within the user gallery
+current_image = 0
+
+lfsr = 1
+tap = 0xdc29
+
+
+# Display an error msg on screen and keep it looping
+def display_error(text):
+    while 1:
+        for i in range(2):
+            display.set_layer(i)
+            display.set_pen(BACKGROUND)
+            display.clear()
+        display.set_pen(WHITE)
+        display.text(f"Error: {text}", 10, 10, WIDTH - 10, 1)
+        presto.update()
+        time.sleep(1)
+
+
+try:
+    # Setup for SD Card
+    sd_spi = machine.SPI(0, sck=machine.Pin(34, machine.Pin.OUT), mosi=machine.Pin(35, machine.Pin.OUT), miso=machine.Pin(36, machine.Pin.OUT))
+    sd = sdcard.SDCard(sd_spi, machine.Pin(39))
+
+    # Mount the SD to the directory 'sd'
+    uos.mount(sd, "/sd")
+
+    # if the gallery folder exists on the SD card we want to use the images in there!
+    if os.stat('sd/gallery'):
+        directory = 'sd/gallery'
+
+except OSError:
+    pass
+
+
+def numberedfiles(k):
+    try:
+        return int(k[:-4])
+    except ValueError:
+        pass
+    return 0
+
+
+try:
+    files = list(file for file in sorted(os.listdir(directory), key=numberedfiles) if file.endswith('.jpg'))
+except OSError:
+    display_error("Problem loading images.\n\nEnsure that your Presto or SD card contains a 'gallery' folder in the root")
+
+total_image_count = len(files) - 1
+
+
+def return_point():
+    global lfsr
+
+    x = lfsr & 0x00ff
+    y = (lfsr & 0xff00) >> 8
+
+    lsb = lfsr & 1
+    lfsr >>= 1
+
+    if lsb:
+        lfsr ^= tap
+
+    if x - 1 < 240 and y < 240:
+        return x - 1, y
+
+    return -1, -1
+
+
+def fizzlefade():
+    display.set_pen(BLACK)
+    display.set_layer(1)
+
+    while True:
+
+        for i in range(2000):
+            x, y = return_point()
+            if x > -1 and y > -1:
+                display.pixel(x, y)
+            if lfsr == 1:
+                break
+
+        presto.update()
+        if lfsr == 1:
+            break
+
+
+def show_image(show_next=False, show_previous=False):
+    global current_image
+    global total_image_count
+
+    # Get the next image in the gallery
+    # If we're at the end of the gallery, loop back and start from 1.
+    if show_next:
+        if current_image < total_image_count:
+            current_image += 1
+        else:
+            current_image = 0
+    if show_previous:
+        if current_image > 0:
+            current_image -= 1
+        else:
+            current_image = total_image_count
+
+    # Open the index file and read lines until we're at the correct position
+    try:
+        img = f"{directory}/{files[current_image]}"
+
+        j.open_file(img)
+
+        img_height, img_width = j.get_height(), j.get_width()
+
+        img_x = 0
+        img_y = 0
+
+        # if the image isn't exactly 240x240 then we'll try to centre the image
+        if img_width < WIDTH:
+            img_x = (WIDTH // 2) - (img_width // 2)
+
+        if img_height < HEIGHT:
+            img_y = (HEIGHT // 2) - (img_height // 2)
+
+        display.set_layer(0)
+        display.set_pen(BACKGROUND)
+        display.clear()
+        j.decode(img_x, img_y, jpegdec.JPEG_SCALE_FULL, dither=True)
+
+        # fizzlefade()
+
+        # Now draw the current image to Layer 1
+        display.set_layer(1)
+        # Decode the JPEG
+        j.decode(img_x, img_y, jpegdec.JPEG_SCALE_FULL, dither=True)
+
+    except OSError:
+        display_error("Unable to find/read file.\n\nCheck that the 'gallery' folder in the root of your SD card contains JPEG images!")
+    except IndexError:
+        display_error("Unable to read images in the 'gallery' folder.\n\nCheck the files are present and are in JPEG format.")
+
+
+def clear():
+    display.set_pen(BACKGROUND)
+    display.set_layer(0)
+    display.clear()
+    display.set_layer(1)
+    display.clear()
+
+
+# Store the last time the screen was updated
+last_updated = time.time()
+
+# Show the first image on the screen so it's not just noise :)
+# We're not passing the arg for 'show_next' or 'show_previous' so it'll show whichever image is current
+clear()
+show_image()
+presto.update()
+
+# FIXME: we should be able to toggle
+presto.set_backlight(0.1)
+
+while True:
+
+    # Poll the touch so we can see if anything changed since the last time
+    touch.poll()
+
+    # Check if it's time to update the image!
+    if time.time() - last_updated > INTERVAL:
+
+        last_updated = time.time()
+        show_image(show_next=True)
+        presto.update()
+
+    # if the screen is reporting that there is touch we want to handle that here
+    if touch.state:
+        # Right half of the screen moves to the next image
+        # The LEDs on the right side of the presto light up to show it is working
+        if touch.x > WIDTH // 2:
+            #for i in LEDS_RIGHT:
+            #    presto.set_led_rgb(i, 255, 255, 255)
+            show_image(show_next=True)
+            presto.update()
+            last_updated = time.time()
+            #for i in LEDS_RIGHT:
+            #    presto.set_led_rgb(i, 0, 0, 0)
+            time.sleep(0.01)
+
+        # Left half of the screen moves to the previous image
+        elif touch.x < WIDTH // 2:
+            for i in LEDS_LEFT:
+                presto.set_led_rgb(i, 255, 255, 255)
+            show_image(show_previous=True)
+            presto.update()
+            last_updated = time.time()
+            for i in LEDS_LEFT:
+                presto.set_led_rgb(i, 0, 0, 0)
+            time.sleep(0.01)
+
+        # Wait here until the user stops touching the screen
+        while touch.state:
+            touch.poll()
+            time.sleep(0.02)
---
+++ tweaked/word_clock.py
@@ -0,0 +1,164 @@
+# ICON [[(6.59, 7.4), (9.39, 4.6), (1.99, -2.8), (1.99, -12.0), (-2.01, -12.0), (-2.01, -1.2), (6.59, 7.4)], [(-0.01, 18.0), (-2.77, 17.82), (-5.22, 17.33), (-6.81, 16.84), (-9.0, 15.88), (-10.82, 14.83), (-12.37, 13.73), (-13.38, 12.88), (-14.8, 11.47), (-16.53, 9.28), (-17.71, 7.33), (-18.44, 5.84), (-18.93, 4.56), (-19.44, 2.82), (-19.69, 1.62), (-19.93, -0.24), (-19.98, -3.03), (-19.82, -4.82), (-19.36, -7.14), (-18.78, -8.99), (-18.18, -10.41), (-16.87, -12.77), (-15.61, -14.52), (-14.53, -15.77), (-13.03, -17.19), (-11.75, -18.19), (-9.49, -19.6), (-7.63, -20.48), (-5.31, -21.29), (-2.8, -21.81), (-1.17, -21.97), (0.56, -22.0), (2.17, -21.89), (4.17, -21.57), (5.78, -21.15), (6.98, -20.74), (8.54, -20.07), (10.61, -18.95), (12.5, -17.62), (14.56, -15.73), (15.71, -14.38), (16.82, -12.81), (18.11, -10.45), (18.75, -8.94), (19.3, -7.26), (19.84, -4.56), (19.98, -2.76), (19.98, -1.18), (19.8, 0.82), (19.39, 2.89), (18.67, 5.12), (17.97, 6.73), (16.56, 9.2), (15.45, 10.7), (13.58, 12.69), (11.88, 14.09), (10.45, 15.06), (9.16, 15.79), (6.7, 16.87), (5.01, 17.38), (2.25, 17.88), (0.04, 18.0)], [(-0.01, -2.0)], [(-0.01, 14.0), (1.87, 13.9), (3.1, 13.72), (4.92, 13.27), (6.57, 12.65), (7.85, 12.0), (9.95, 10.56), (11.26, 9.38), (12.07, 8.51), (13.65, 6.4), (14.66, 4.51), (15.18, 3.17), (15.75, 0.9), (15.93, -0.48), (15.99, -2.41), (15.75, -4.87), (15.46, -6.25), (14.87, -8.01), (14.31, -9.23), (13.28, -10.95), (12.42, -12.08), (11.05, -13.55), (9.91, -14.56), (8.05, -15.86), (6.45, -16.69), (4.54, -17.39), (3.36, -17.68), (1.71, -17.92), (0.44, -18.0), (-1.44, -17.94), (-2.97, -17.75), (-5.29, -17.16), (-6.71, -16.59), (-8.07, -15.88), (-10.05, -14.49), (-11.32, -13.34), (-12.48, -12.07), (-13.2, -11.12), (-14.1, -9.69), (-14.72, -8.44), (-15.33, -6.79), (-15.77, -4.91), (-15.98, -3.05), (-16.0, -1.85), (-15.9, -0.04), (-15.44, 2.39), (-14.95, 3.89), (-14.24, 5.45), (-13.24, 7.08), (-12.22, 8.41), (-11.39, 9.31), (-10.07, 10.49), (-8.57, 11.58), (-7.27, 12.32), (-5.83, 12.96), (-4.11, 13.51), (-1.72, 13.91), (-0.06, 14.0)]]
+# NAME Word Clock
+# DESC No hands!
+
+import time
+
+import machine
+import ntptime
+import pngdec
+from presto import Presto
+
+# Setup for the Presto display
+presto = Presto()
+display = presto.display
+WIDTH, HEIGHT = display.get_bounds()
+BLACK = display.create_pen(0, 0, 0)
+WHITE = display.create_pen(200, 200, 200)
+GRAY = display.create_pen(30, 30, 30)
+
+# Clear the screen before the network call is made
+display.set_pen(BLACK)
+display.clear()
+presto.update()
+
+# Length of time between updates in minutes.
+UPDATE_INTERVAL = 15
+
+# Timezone offset, can be + or -
+UTC_OFFSET = 1
+
+rtc = machine.RTC()
+time_string = None
+words = ["it", "d", "is", "m", "about", "lv", "half", "c", "quarter", "b", "to", "past", "n", "one",
+         "two", "three", "four", "five", "six", "eleven", "ten", "d", "qdh", "eight", "seven", "rm", "twelve", "nine", "p", "ncsnheypresto", "O'Clock", "agrdsp"]
+
+
+def show_message(text):
+    display.set_pen(BLACK)
+    display.clear()
+    display.set_pen(WHITE)
+    display.text(f"{text}", 5, 10, WIDTH, 2)
+    presto.update()
+
+
+show_message("Connecting...")
+
+try:
+    wifi = presto.connect()
+except ValueError as e:
+    while True:
+        show_message(e)
+except ImportError as e:
+    while True:
+        show_message(e)
+
+# Set the correct time using the NTP service.
+try:
+    ntptime.settime()
+except OSError:
+    while True:
+        show_message("Unable to get time.\n\nCheck your network try again.")
+
+
+def approx_time(hours, minutes):
+    nums = {0: "twelve", 1: "one", 2: "two",
+            3: "three", 4: "four", 5: "five", 6: "six",
+            7: "seven", 8: "eight", 9: "nine", 10: "ten",
+            11: "eleven", 12: "twelve"}
+
+    hours=hours+1
+    if hours == 12:
+        hours = 0
+    if minutes > 0 and minutes < 8:
+        return "it is about " + nums[hours] + " O'Clock"
+    elif minutes >= 8 and minutes < 23:
+        return "it is about quarter past " + nums[hours]
+    elif minutes >= 23 and minutes < 38:
+        return "it is about half past " + nums[hours]
+    elif minutes >= 38 and minutes < 53:
+        return "it is about quarter to " + nums[hours + 1]
+    else:
+        return "it is about " + nums[hours + 1] + " O'Clock"
+
+
+def update():
+    global time_string
+    # grab the current time from the ntp server and update the Pico RTC
+    try:
+        ntptime.settime()
+    except OSError:
+        print("Unable to contact NTP server")
+
+    current_t = rtc.datetime()
+    #current_t = time.gmtime(time.time() + UTC_OFFSET * 60 * 60)
+
+    time_string = approx_time(current_t[4] - 12 if current_t[4] > 12 else current_t[4], current_t[5])
+
+    # Splits the string into an array of words for displaying later
+    time_string = time_string.split()
+
+    print(time_string)
+
+
+def draw():
+    global time_string
+    display.set_font("bitmap8")
+
+    display.set_layer(1)
+
+    # Clear the screen
+    display.set_pen(BLACK)
+    display.clear()
+
+    default_x = 25
+    x = default_x
+    y = 35
+
+    line_space = 20
+    letter_space = 15
+    margin = 25
+    scale = 1
+    spacing = 1
+
+    for word in words:
+
+        if word in time_string:
+            display.set_pen(WHITE)
+        else:
+            display.set_pen(GRAY)
+
+        for letter in word:
+            text_length = display.measure_text(letter, scale, spacing)
+            if not x + text_length <= WIDTH - margin:
+                y += line_space
+                x = default_x
+
+            display.text(letter.upper(), x, y, WIDTH, scale=scale, spacing=spacing)
+            x += letter_space
+
+    presto.update()
+
+
+# Set the background in layer 0
+# This means we don't need to decode the image every frame
+
+display.set_layer(0)
+
+try:
+    p = pngdec.PNG(display)
+
+    p.open_file("wordclock_background.png")
+    p.decode(0, 0)
+except OSError:
+    display.set_pen(BLACK)
+    display.clear()
+
+
+# FIXME: we should be able to toggle
+presto.set_backlight(0.5)
+
+while True:
+    update()
+    draw()
+    time.sleep(60 * UPDATE_INTERVAL)
